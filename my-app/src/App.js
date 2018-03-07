0c7e9736e2c7b5892f441865f06fbc9f28aad66e
CunxiaZhang <252946169@qq.com>
2018-03-07 16:14:33 +0800

    initial

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,19 @@
 import React, { Component } from 'react';
-import { BrowserRouter, Route,Link} from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
-
-import Home from './components/Home';
-import Product from './components/Product';
-import About from './components/About';
 
 class App extends Component {
   render() {
     return (
-      <BrowserRouter>
-            <div>
-              <a href="/abc">家</a>
-              <a href="/abc1">产品</a>
-              <a href="/abc2">我们</a>
-              <br/>
-              <Link to="abc"> 家</Link>
-              <Link to="abc1"> 产品</Link>
-              <Link to="abc2">我们</Link>
-              <br/>
-              <Route path="/abc" component={Home}/>
-              <Route path="/abc1" component={Product}/>
-              <Route path="/abc2" component={About}/>
-            </div>
-
-          </BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+      </div>
     );
   }
 }
