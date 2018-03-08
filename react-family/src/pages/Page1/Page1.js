43d19a4c474341369e9b6f2fadbe2ac4c5dd5d8a
qftgtr <qft.gtr@gmail.com>
2018-03-08 17:37:59 +0800

    编译图片

--- react-family/src/pages/Page1/Page1.js
+++ react-family/src/pages/Page1/Page1.js
@@ -1,11 +1,13 @@
 import React, {Component} from 'react';
 import './Page1.css';
+import image from './images/bg1.png';
 
 export default class Page1 extends Component {
     render() {
         return (
             <div className="page-box">
                 this is Page1111~~~~~~~~~
+                <img src={image}/>
             </div>
         )
     }
--- react-family/webpack.dev.config.js
+++ react-family/webpack.dev.config.js
@@ -25,7 +25,16 @@ module.exports = {
          {
            test: /\.css$/,
            use: ['style-loader', 'css-loader']
-         }
+         },
+         {
+            test: /\.(png|jpg|gif)$/,
+            use: [{
+                loader: 'url-loader',
+                options: {
+                    limit: 8192
+                }
+            }]
+          }
        ]
     },
     devServer: {
