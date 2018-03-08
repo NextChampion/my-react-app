a03f4e9137b559504678c174810ad99a38ef7178
qftgtr <qft.gtr@gmail.com>
2018-03-08 16:18:56 +0800

    配置webpack-dev-server

--- react-family/webpack.dev.config.js
+++ react-family/webpack.dev.config.js
@@ -18,5 +18,8 @@ module.exports = {
            use: ['babel-loader?cacheDirectory=true'],
            include: path.join(__dirname, 'src')
        }]
+    },
+    devServer: {
+        contentBase: path.join(__dirname, './dist')
     }
 };
