d48b7c4d2ac1a478c3d1978aef5a878d91049047
qftgtr <qft.gtr@gmail.com>
2018-03-08 16:36:11 +0800

    添加模块热更新，但是好像没效果啊

--- react-family/src/index.js
+++ react-family/src/index.js
@@ -12,5 +12,9 @@ import Hello from './component/Hello/Hello';
 
 import getRouter from './router/router';
 
+if (module.hot) {
+    module.hot.accept();
+}
+
 ReactDom.render(
     getRouter(), document.getElementById('app'));
