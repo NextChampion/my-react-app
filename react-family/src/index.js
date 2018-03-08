2b235e75db0d9e25b3bf72b9ba08345bc6881f4b
qftgtr <qft.gtr@gmail.com>
2018-03-08 17:09:02 +0800

    添加counter页面并使用redux实现计数功能

--- react-family/src/index.js
+++ react-family/src/index.js
@@ -10,7 +10,8 @@ import React from 'react';
 import ReactDom from 'react-dom';
 import Hello from './component/Hello/Hello';
 import {AppContainer} from 'react-hot-loader';
-
+import {Provider} from 'react-redux';
+import store from './redux/store';
 import getRouter from 'router/router';
 
 /*初始化*/
@@ -30,7 +31,9 @@ if (module.hot) {
 function renderWithHotReload(RootElement) {
     ReactDom.render(
         <AppContainer>
-            {RootElement}
+            <Provider store={store}>
+                {RootElement}
+            </Provider>
         </AppContainer>,
         document.getElementById('app')
     )
