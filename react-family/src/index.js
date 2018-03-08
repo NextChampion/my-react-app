b7daf99bdd68e2ca50ed9d813ccf6df03b3e7cde
qftgtr <qft.gtr@gmail.com>
2018-03-08 17:32:15 +0800

    编译css:配置并使用css

--- react-family/src/index.js
+++ react-family/src/index.js
@@ -1,11 +1,3 @@
-// document.getElementById('app').innerHTML = "Webpack workss"
-
-/*使用es6的箭头函数*/
-// var func = str => {
-//     document.getElementById('app').innerHTML = str;
-// };
-// func('我现在在使用Babel!');
-
 import React from 'react';
 import ReactDom from 'react-dom';
 import Hello from './component/Hello/Hello';
