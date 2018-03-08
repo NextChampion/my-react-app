1393067a9418aefc2b7b75bbf17724159f343d7b
qftgtr <qft.gtr@gmail.com>
2018-03-08 15:23:01 +0800

    使用React

--- react-family/src/index.js
+++ react-family/src/index.js
@@ -1,7 +1,13 @@
 // document.getElementById('app').innerHTML = "Webpack workss"
 
 /*使用es6的箭头函数*/
-var func = str => {
-    document.getElementById('app').innerHTML = str;
-};
-func('我现在在使用Babel!');
+// var func = str => {
+//     document.getElementById('app').innerHTML = str;
+// };
+// func('我现在在使用Babel!');
+
+import React from 'react';
+import ReactDom from 'react-dom';
+
+ReactDom.render(
+    <div>Hello React!</div>, document.getElementById('app'));
